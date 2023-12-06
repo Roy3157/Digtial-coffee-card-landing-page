df34ed940df67e2efeaf0bf3c65006e60d9ba5fd
Alex <gli0471@hotmail.com>
2023-12-06 18:29:52 +1100

    add send email function

---
+++ js/notification.js
@@ -0,0 +1,23 @@
+
+emailjs.init('cRvLZ3RUw3QmirEFF')
+const btn = document.getElementById('btn-submit');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+   btn.disabled = true;
+   btn.innerHTML = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_4jy2yu9';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.innerHTML = 'Submit';
+      btn.disabled = false;
+    }, (err) => {
+      btn.innerHTML = 'Submit';
+      btn.disabled = false;
+      alert(JSON.stringify(err));
+    });
+});
